f28aa2766dd40713f4cb2e6006d56051518e5fa3
tsunami <jvlipnitsk@gmail.com>
2024-11-07 17:17:36 +0900

    modified makeLink

--- makeLink.sh
+++ makeLink.sh
@@ -1,7 +1,11 @@
-cd /dotfiles
+cd dotfiles
 ln -sf .bashrc ~/.bashrc
 ln -sf .zshrc ~/.zshrc
 ln -sf .vimrc ~/.vimrc
 ln -sf .vim/autoload ~/.vim
 ln -sf .vim/doc ~/.vim
 ln -sf .vim/plugin ~/.vim
+mkdir -p .vim/bundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+cd .vim/bundle
+git clone https://github.com/scrooloose/nerdtree.git
